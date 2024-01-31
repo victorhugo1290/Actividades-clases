ef76237be7ad852c1f108916b8573bb497dd52ac
victorhugo1290 <victor.gutierrez8283@alumnos.udg.mx>
2024-01-31 09:43:28 -0600

    co

--- actividad3.cpp
+++ actividad3.cpp
@@ -10,24 +10,21 @@
    #include<iostream>//declarar libreria
 using namespace std;
 int main() {
-    int nume1, contador=0, repetir;//declarar variables  
-    
+    int nume1, contador=0, algo=0, repetir;//declarar variables  
     do {//iniciamos el ciclo
         cout<<"Ingrese un número: ";//le pedimos al usurio el valor a comparar 
         cin>>nume1;
-
         if (nume1 > 0 && nume1 < 500) {//empezara a comparar para verificar que es lo que hara
-            for (int l = 5; l <= 500; l += 5) {
+        for (int l = 5; l <= 500; l += 5) {
                 cout<<l<<endl;
             }
         } else if (nume1 > 500 && nume1 < 1000) {
-            for (int l = 10; l <= 500; l += 10) {
-            cout<<l<<endl;
+        for (int l = 10; l <= 500; l += 10) {
+        cout<<l<<endl;
             }
         } else if (nume1 >- 1000) {
             cout<< "Valor invalido"<<endl;
         }
-
         cout<< "¿Quiere ingresar otro número? 1:Si 2:No" <<endl;
         cin>>repetir;
        contador++;//suma la cantidad de veces que pides los numeros
---
+++ for.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int i=0;
+    int a=1,x;
+    for( i=0;i<=50;i+=5)
+   {
+    x=i+a;
+cout<<"el valor de x es "<<i<<endl;}
+cout<<"el valor de i es"<<x<<endl;
+ /*do{
+    cout<<"hola"<<i<<endl;
+    
+    cout<<i<<endl;
+  }
+while (i<10);*/
+
+
+
+
+
+}
