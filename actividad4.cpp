d992ffbd9272d330a16893513dac2364386aaf74
victorhugo1290 <victor.gutierrez8283@alumnos.udg.mx>
2024-02-04 20:00:08 -0600

    confi

--- actividad4.cpp
+++ actividad4.cpp
@@ -1,65 +1,66 @@
-#include <iostream>
+#include <iostream>//libreria 
 using namespace std;
-int a=1, b=2, c=0, num1, eleg, opc;
-int main (){
+int  a, b, c, num1, eleg, opc;//declaracion de varibles 
+int main (){//incio del programa 
     cout<<"Vamos a hacer la serie de fibonacci"<<endl;
-do
+do//inicio del cliclo para reiniciar el prgrama 
 {
    cout<<"Ingresa un numero"<<endl; 
    cin>>num1;
    cout<<"Que ciclo que quieres utilizar"<<endl;
-   cout<<"For: 1"<<endl;
+   cout<<"For: 1"<<endl;//aqui el usuario decididara con que ciclo quiere ejecutar el problema
    cout<<"While: 2"<<endl;
    cout<<"Do-While: 3"<<endl;
    cin>>eleg;
-   switch (eleg)
+   switch (eleg)//inicio del switch por la eleccion del usuario 
    {
-   case 1:
+   case 1://caso uno
     cout<<"La serie hasta la cantidad que ingresaste es:"<<endl;
     cout<<"0"<<endl;
-    cout<<"1"<<endl;
-    cout<<"1"<<endl;
-    for ( c = 0; c <= num1;)
+    a=0;//la variable "a" iniciara como 0
+    b=1;//la variable "b" iniciara como 1
+    for ( c = 0; c <= num1;)//se repetira el cliclo miestras que c sea menor o igual que el numero que puso el usuario 
     {
-        c=a+b;
-        a=b;
-        b=c;
-        cout<<a<<endl;
+        c=a+b;//"c" sera la suma de "a" mas "b"
+        a=b;//"a" pasara a tomar el valor de "B"
+        b=c;//"b" pasara a tomar el valor de "c"
+        cout<<a<<endl;//se mostrara el respectivo valor de "a" en cada ciclo 
     }
     break;
    case 2:
    cout<<"La serie hasta la cantidad que ingresaste es:"<<endl;
     cout<<"0"<<endl;
-    cout<<"1"<<endl;
-    cout<<"1"<<endl;
     c=0;
-    while (c<=num1)
+    a=0;//la variable "a" iniciara como 0
+    b=1;//la variable "b" iniciara como 1
+    while (c<=num1)//miestras c sea menor o igual al numero ingresado por el usurio se repetira el usuario
     {
-         c=a+b;
-        a=b;
-        b=c;
-        cout<<a<<endl;
+        c=a+b;//"c" sera la suma de "a" mas "b"
+        a=b;//"a" pasara a tomar el valor de "B"
+        b=c;//"b" pasara a tomar el valor de "c"
+        cout<<a<<endl;//se mostrara el respectivo valor de "a" en cada ciclo 
     }
     break;
     case 3:
     cout<<"La serie hasta la cantidad que ingresaste es:"<<endl;
     cout<<"0"<<endl;
-    cout<<"1"<<endl;
-    cout<<"1"<<endl;
     c=0;
-    do
+    a=0;//la variable "a" iniciara como 0
+    b=1;//la variable "b" iniciara como 1
+    do//inicio del el ciclo 
     {
-        c=a+b;
-        a=b;
-        b=c;
-        cout<<a<<endl;
-    } while (c<=num1);
+        c=a+b;//"c" sera la suma de "a" mas "b"
+        a=b;//"a" pasara a tomar el valor de "B"
+        b=c;//"b" pasara a tomar el valor de "c"
+        cout<<a<<endl;//se mostrara el respectivo valor de "a" en cada ciclo 
+    } while (c<=num1);//el cliclo acabara cuando c sea mayor que el numero ingresado por el usuario
+    break;
    }
-   cout<<"Quieres ingresar otro numero?"<<endl;
+    cout<<"Quieres ingresar otro numero?"<<endl;
     cout<<"1: Si"<<endl;
     cout<<"2: NO"<<endl;
     cin>>opc;
-    
-} while (opc=1);
+} while (opc==1);
 cout<<"Perfecto, Hasta luego"<<endl;
+
 }
