dffe172a634eccd76416807bc81653ce3b32b511
victorhugo1290 <victor.gutierrez8283@alumnos.udg.mx>
2024-04-21 19:53:25 -0600

    confirmar

--- codigoclase.cpp
+++ codigoclase.cpp
@@ -1,13 +1,175 @@
-
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>//Librer�as
+
+int lugar, suma=0, num, valor, a, n;//variables enteras
+int myvector[50]; // Vector de 10 elementos inicializados en 0
+
 using namespace std;
-const int n=2;
-#include "libreriaxd.cpp"
-int main(){
-srand((unsigned)time(NULL));
-int mat[n][n]={{10,2},{5,6}};
-//mat[2][2]={{10,2},[5,6]};
-llenar(mat,n);
-printx(mat,n);
+
+int particion(int myvector[], int inicio, int fin) {
+    int pivote = myvector[fin];
+    int i = (inicio - 1);
+
+    for (int j = inicio; j <= fin - 1; j++) {
+        if (myvector[j] < pivote) {
+            i++;
+            swap(myvector[i], myvector[j]);
+        }
+    }
+    swap(myvector[i + 1], myvector[fin]);
+    return (i + 1);
 }
+///using namespace std;
+void lista(){
+    cout<<"As� queda la lista de valores que proporcionaste"<<endl;
+		    for(int a=0; a<50; a++){
+		    cout<<myvector[a]<<endl;//Se imprime todos los valores que ingresamos a myvector
+      }
+}
+
+
+void editar(){
+     cout<<"Ingresa  la posici�n que deseas modificar o borrar empezando del 0-9"<<endl;
+		    cin>>lugar;//varible de posici�n
+		    if (lugar >=0 && lugar < 50){//rango de posici�n
+			cout<<"Ahora pon tu n�mero nuevo"<<endl;
+			cin>>num;//variable de nuevo n�mero
+			myvector[lugar]=num;}//se determina
+     }
+
+void vaciar(){
+     for (int i=0; i<50; ++i) {
+        myvector[i]=0;
+    }
+	 cout<<"Ha quedado vacío"<<endl;
+     }
+
+void quicksort(int myvector[], int inicio, int fin) {
+    if (inicio < fin) {
+        int indiceParticion = particion(myvector, inicio, fin);
+        quicksort(myvector, inicio, indiceParticion - 1);
+        quicksort(myvector, indiceParticion + 1, fin);
+    }
+}
+void burbuja(int myvector[], int tamano) {
+    for (int i = 0; i < tamano - 1; ++i) {
+        for (int j = 0; j < tamano - i - 1; ++j) {
+            if (myvector[j] > myvector[j + 1]) {
+                swap(myvector[j], myvector[j + 1]);
+            }
+        }
+    }
+}
+void randomnum() {
+    srand(time(nullptr)); // Inicializar la semilla del generador de números aleatorios
+
+    for (int i = 0; i < 50; ++i) {
+        myvector[i] = rand() % 200 + 1; // Generar números aleatorios entre 1 y 200
+    }
+}
+int busquedaBinaria(int myvector[], int valor, int tamano) {
+    int inicio = 0;
+    int fin = tamano - 1;
+
+    while (inicio <= fin) {
+        int medio = inicio + (fin - inicio) / 2;
+
+        if (myvector[medio] == valor) {
+            cout << "El valor " << valor << " se encuentra en la posición " << medio << " del arreglo." << endl;
+            return medio;
+        }
+
+        if (myvector[medio] < valor) {
+            inicio = medio + 1;
+        } else {
+            fin = medio - 1;
+        }
+    }
+
+    cout << "El valor " << valor << " no se encuentra en el arreglo." << endl;
+    return -1;
+}
+int main (){//inicio del programa
+    char letra;
+    int accion;
+	int pos=0;
+
+    cout<<"Hola, hola, bienvenid@ a este programa para crear tus valores en el arreglo, Comencemos!!"<<endl;
+   do{//ciclo
+   
+        cout<<"Ingresa la posici�n que deseas modificar o borrar empezando del 0-49"<<endl;
+		    cin>>lugar;//variable para determinar la posici�n
+            do{//LIZ ESTE DO ES PARA QUE SE REPITA SI PONEN UN NUMERO MAYOR A 200
+		    if (lugar >= 0 && lugar<50){//rango de posici�n
+			cout<<"Ahora pon el n�mero que deseas"<<endl;
+			cin>>num;//variable del n�mero que se desea agregar
+			myvector[lugar]=num;}
+            if (num > 200)
+            cout<<"Valor invalido"<<endl;
+   }while(num>200);
+            cout<<"deseas seguir escribir 's' para seguir o 'n' para terminar"<<endl;
+            cin>>letra;//determinar si se quieren o no seguir agregando n�meros
+            if(letra=='s')//en caso de si querer
+            cout<<"continuemos!!"<<endl;
+            if (letra=='n')//en caso de no querer
+            cout<<"Iremos al men�..."<<endl;
+   }while(letra!='n');//condici�n para salir del ciclo
+
+do{//comienzo del ciclo repetir
+	cout<<"Selecciona la acci�n que le deseas aplicar al arreglo, ingresa el n�mero de lo que deseas hacer:\n"
+	"1-Mostrar la lista de valores\n"
+    "2-Mostrar sumatoria de todos los elementos\n"
+    "3-Borrado y edici�n de elementos\n""4-Vaciado del arreglo\n""5-Autollenado\n""6-Ordenamiento burbuja\n"
+    "7-Ordenamiento Quicksort\n""8-Busqueda Binaria\n""9-Salir del programa"<<endl;//Men� de acciones
+	cin>>accion;//Se lee lo que se desea hacer
+	switch (accion) {//inicio del switch
+
+		case 1://primer caso (mostrar la lista)
+		    lista();
+			break;
+
+		case 3://tercer caso(editar o eliminar)
+            editar();
+			break;
+
+		case 4://cuarto caso(vaciado)
+		    vaciar();
+			break;
+
+        case 5://quinto caso(autollenado)
+            randomnum();
+			cout<<"El arreglo ya ha sido rellenado con valores aleatorios entre el 1 y el 200"<<endl;
+            break;
+
+		case 6://ordenamiento burbuja
+		burbuja(myvector, 50);
+                cout << "El arreglo fue ordenado por método de burbuja" << endl;
+                break;
+
+    	case 7: //ordenamiento quicksort 
+		  quicksort(myvector, 0, 49);
+    	  cout<<"Arreglo ordenado con el método Quicksort."<<endl;
+   		 break;
+		
+		case 8:
+        cout << "Ingresa el valor que deseas buscar:" << endl;
+                cin >> num;
+                if (busquedaBinaria(myvector, num, 50) != -1) {
+                    cout << "El valor " << num << " se encuentra en el arreglo." << endl;
+                } else {
+                    cout << "El valor " << num << " no se encuentra en el arreglo." << endl;
+                }
+    		break;
+
+		case 9: //sexto caso (salir del programa)
+			cout<<"Gracias por tu ayuda, nos vemos luego ;) "<<endl;//despedida
+			break;
+		default:
+            cout<<"Selección inválida. Por favor, selecciona un número del 1 al 6."<<endl;
+            break;
+        }
+    } while (accion!=9);//Fin del ciclo "do" ponemos la condici�n de ingresar el caso 5
+
+	}//fin del programa
--- libreriaxd.cpp
+++ libreriaxd.cpp
@@ -1,21 +1,230 @@
+#include <iostream>//libreria
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-void llenar(int ar[][n], int tam){
-for (int f = 0; f < tam; f++)
-{
-    for (int c = 0; c < tam; c++)
-    {
-        ar[f][c]=-100+rand()%300;
+// Declaración de variables 
+int pos=0,e,b, arreglo[50], num;
+char eleccion;
+bool flag = true;//funcion para repetir el programa
+int particion(int arreglo[], int inicio, int fin) {
+    int pivote = arreglo[fin];
+    int i = (inicio - 1);
+
+    for (int j = inicio; j <= fin - 1; j++) {
+        if (arreglo[j] < pivote) {
+            i++;
+           swap(arreglo[i], arreglo[j]);
+        }
     }
-    
+    swap(arreglo[i + 1], arreglo[fin]);
+    return (i + 1);
 }
-}
-void printx(int ar[][n], int tam){
-for (int f = 0; f < tam; f++)
-{
-    for (int c = 0; c < tam; c++)
-    {
-        cout<<"["<<ar[f][c]<<"]\t";
+
+void quicksort(int arreglo[], int inicio, int fin) {
+    if (inicio < fin) {
+        int indiceParticion = particion(arreglo, inicio, fin);
+        quicksort(arreglo, inicio, indiceParticion - 1);
+        quicksort(arreglo, indiceParticion + 1, fin);
     }
-    cout<<endl;
 }
+void ordenamientoBurbuja(int arreglo[], int tamano) {
+    for (int i = 0; i < tamano - 1; ++i) {
+        for (int j = 0; j < tamano - i - 1; ++j) {
+            if (arreglo[j] > arreglo[j + 1]) {
+                swap(arreglo[j], arreglo[j + 1]);
+            }
+        }
+    }
+}
+void borrar(){
+     if (pos == 0) {
+    cout <<"No tiene elementos"<<endl;
+    } 
+    else {
+    cout<<"Ingrese el índice del elemento para borrar 1,10"<<endl;
+    for (int i = 0; i < pos; i++) {
+    cout<<i+1<<":"<<arreglo[i]<< endl;
+    }
+    cout << endl;
+    cout << "Seleccione una opción: ";
+    cin >> b;
+    b = b - 1;
+    arreglo[b] = 0;
+    cout << "Elemento borrado" << endl;
+    }
+}
+void vaciado(){
+     if (pos == 0) {
+    cout << "Ya se encuentra Vacio"<<endl;
+    } 
+    else {
+    for (int i = 0; i < pos; i++) 
+    {
+    arreglo[i] = 0;
+    }
+    pos = 0;
+    cout << "Elementos borrados"<<endl;
+    }
+}
+void editar(){
+    if (pos == 0) {
+    cout<<"No hay valores"<<endl;
+    } 
+    else {
+    cout << "Que valor desea editar"<<endl;
+    for (int i = 0; i < pos; i++) {
+    cout <<i+1<<":"<<arreglo[i]<<endl;
+        }
+    cout << endl;
+    cout << "Seleccione: ";
+    cin >> e;
+    e = e - 1;
+    cout << "Ingrese el nuevo numero ";
+    cin >> arreglo[e];
+    if(arreglo[e]>200){
+        cout<<"Valor incorrecto"<<endl;
+        e-1;
+        
+    }
+    cout<<"Valor editado"<<endl;
+    }
+}
+void lista(){
+if (pos == 0) {
+    cout <<"NO tiene valores que mostrar"<<endl;
+    } 
+    else {
+    cout << endl;
+    for (int i = 0; i < pos; i++) {
+    cout<<arreglo[i]<<endl;
+    }
+    cout << endl;
+    }
+}
+void valoresarreglo(){
+    if (pos == 50) {
+    cout << "Ya esta completo"<<endl;
+    } else {
+         cout<<"Ingrese el numero"<<" "<<pos+1<<endl;
+    cin >> arreglo[pos];
+        if (arreglo[pos]<200)
+        {
+                
+    pos++;
+    }
+    else{
+         cout<<"valor invalido"<<endl;
+    }
+        }
+
+       
+}
+void aleatorio(){
+    // Semilla para la generación de números aleatorios
+    std::srand(std::time(nullptr));
+
+    // Generar números aleatorios y llenar el arreglo
+    for (arreglo[pos]; arreglo[pos] < 50 ; pos++) {
+        arreglo[pos] = std::rand() % 201; // Genera un número entre 0 y 200
+    }
+
+    std::cout << "Arreglo llenado con números aleatorios." << std::endl;
+}
+int busquedaBinaria(int arreglo[], int valor, int tamano) {
+    int inicio = 0;
+    int fin = tamano - 1;
+
+    while (inicio <= fin) {
+        int medio = inicio + (fin - inicio) / 2;
+
+        if (arreglo[medio] == valor) {
+            cout << "El valor " << valor << " se encuentra en la posición " << medio << " del arreglo." << endl;
+            return medio;
+        }
+
+        if (arreglo[medio] < valor) {
+            inicio = medio + 1;
+        } else {
+            fin = medio - 1;
+        }
+    }
+
+    cout << "El valor " << valor << " no se encuentra en el arreglo." << endl;
+    return -1;
+}
+void menu(){
+    cout<<"Bienvenido"<<endl;
+cout << "\n--- Menú ---" << endl;
+cout << "1. Ingresar un valor" << endl;
+cout << "2. Mostrar lista de valores" << endl;
+cout << "3. Llenar con numeros aleatorios"<<endl;
+cout << "4. Ordenar el arreglo con Quicksort" << endl;
+cout << "5. Ordenar el arreglo con Burbuja" << endl;
+cout << "6. Busqueda binaria" <<endl;
+cout << "7. Editar un elemento" << endl;
+cout << "8. Vaciar arreglo" << endl;
+cout << "9. Salir"<<endl;
+    cin >> eleccion;
+}
+// Función principal
+int main() {
+    // Bucle principal del programa
+while (flag) {
+// Menú de opciones
+menu();
+// Switch para manejar las opciones del menú
+    switch (eleccion) {
+case '1':
+    // Ingresar valores al arreglo
+    valoresarreglo();
+    break;
+ case '2':
+    // Mostrar lista de valores en el arreglo
+    lista();
+    break;
+case '3':
+// Llenar con numeros aleatorios
+    aleatorio();
+    break; 
+case '4':
+// Ordenar el arreglo con Quicksort
+    quicksort(arreglo, 0, pos - 1);
+    cout<<"Arreglo ordenado con el método Quicksort."<<endl;
+    break;
+case '5':
+ // Ordenar el arreglo con el método de burbuja
+    ordenamientoBurbuja(arreglo, pos);
+    cout<< "El arreglo fue ordenado por metodo de burbuja"<< endl;
+    break;
+case '6':
+cout << "Ingresa el valor que deseas buscar:" << endl;
+                cin >> num;
+                if (busquedaBinaria(arreglo, num, 50) != -1) {
+                    cout << "El valor " << num << " se encuentra en el arreglo." << endl;
+                } else {
+                    cout << "El valor " << num << " no se encuentra en el arreglo." << endl;
+                }
+break;
+case '7':
+  // Editar un elemento del arreglo
+ editar();
+    break; 
+case '8':
+    // Vaciar completamente el arreglo
+    vaciado();
+    break;
+case '9':
+    // Salir del programa
+    flag = false;
+        break;
+default:
+    // Opción incorrecta en el menú
+    cout<<"Opcion incorrecta"<<endl;
+    break;
+        }
+    } 
+
+    // Mensaje de despedida al salir del programa
+    cout<<"Gracias por usar el programa"<<endl;
 }
